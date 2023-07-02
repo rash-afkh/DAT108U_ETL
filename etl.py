f8329282b65895b2a3b0204ed9f091614b096814
Rashid-Afkhami <rash.afkhami@gmail.com>
2023-07-02 21:32:03 +0800

    working version

--- etl.py
+++ etl.py
@@ -7,37 +7,40 @@ from sql_queries import *
 
 def process_song_file(cur, filepath):
     # open song file
-    df = 
+    df = pd.read_json(filepath, lines=True)
 
     # insert song record
-    song_data = 
+    song_data_columns = ['song_id', 'title', 'artist_id', 'year', 'duration']
+    song_data = df[song_data_columns].values[0]
     cur.execute(song_table_insert, song_data)
     
     # insert artist record
-    artist_data = 
+    artist_data_columns = ['artist_id', 'artist_name', 'artist_location', 'artist_latitude', 'artist_longitude']
+    artist_data = df[artist_data_columns].values[0]
     cur.execute(artist_table_insert, artist_data)
 
 
 def process_log_file(cur, filepath):
     # open log file
-    df = 
+    df = pd.read_json(filepath, lines=True)
 
     # filter by NextSong action
-    df = 
+    df = df.loc[df['page'] == 'NextSong']
 
     # convert timestamp column to datetime
-    t = 
+    t = pd.to_datetime(df['ts'], unit='ms')
     
     # insert time data records
-    time_data = 
-    column_labels = 
-    time_df = 
+    time_data = [t.dt.hour.to_list(), t.dt.day_of_week.to_list(), t.dt.isocalendar().week.to_list(), 
+                 t.dt.month.to_list(), t.dt.year.to_list()]
+    column_labels = ['hour', 'day_of_week', 'week', 'month', 'year']
+    time_df = pd.DataFrame(dict(zip(column_labels, time_data)))
 
     for i, row in time_df.iterrows():
         cur.execute(time_table_insert, list(row))
 
     # load user table
-    user_df = 
+    user_df = df[['userId', 'firstName', 'lastName', 'gender', 'level']]
 
     # insert user records
     for i, row in user_df.iterrows():
@@ -56,7 +59,8 @@ def process_log_file(cur, filepath):
             songid, artistid = None, None
 
         # insert songplay record
-        songplay_data = 
+        songplay_data = (row.ts, row.userId, row.level, songid, artistid, 
+                     row.sessionId, row.location, row.userAgent) 
         cur.execute(songplay_table_insert, songplay_data)
 
 
--- sql_queries.py
+++ sql_queries.py
@@ -1,52 +1,101 @@
-# DROP TABLES
-
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
-
-# CREATE TABLES
-
-songplay_table_create = ("""
-""")
-
-user_table_create = ("""
-""")
-
-song_table_create = ("""
-""")
-
-artist_table_create = ("""
-""")
-
-time_table_create = ("""
-""")
-
-# INSERT RECORDS
-
-songplay_table_insert = ("""
-""")
-
-user_table_insert = ("""
-""")
-
-song_table_insert = ("""
-""")
-
-artist_table_insert = ("""
-""")
-
-
-time_table_insert = ("""
-""")
-
-# FIND SONGS
-
-song_select = ("""
-""")
-
-# QUERY LISTS
-
-create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+gender_type = """DO $$ BEGIN 
+                    CREATE TYPE gender_type AS ENUM ('F', 'M'); 
+                EXCEPTION 
+                    WHEN duplicate_object THEN null; 
+                END $$; 
+""" 
+ 
+# DROP TABLES 
+def drop_table(name): 
+    return f"DROP TABLE IF EXISTS {name};" 
+ 
+songplay_table_drop = drop_table('songplay') 
+user_table_drop = drop_table('users') 
+song_table_drop = drop_table('song') 
+artist_table_drop = drop_table('artist') 
+time_table_drop = drop_table('time') 
+ 
+# CREATE TABLES 
+ 
+songplay_table_create = ("""CREATE TABLE songplay ( 
+        timestamp NUMERIC(13,0), 
+        user_id INT, 
+        level VARCHAR(10), 
+        song_id VARCHAR(25), 
+        artist_id VARCHAR(25), 
+        session_id INT, 
+        location VARCHAR(55), 
+        user_agent VARCHAR(250) 
+        ); 
+""") 
+ 
+user_table_create = (gender_type + """CREATE TABLE users ( 
+        user_id INT, 
+        first_name VARCHAR(35), 
+        last_name VARCHAR(35), 
+        gender gender_type, 
+        level VARCHAR(10) 
+        ); 
+""") 
+ 
+song_table_create = ("""CREATE TABLE song ( 
+        song_id VARCHAR(25), 
+        title VARCHAR(100), 
+        artist_id VARCHAR(25), 
+        duration FLOAT4, 
+        year INT 
+        ); 
+""") 
+ 
+artist_table_create = ("""CREATE TABLE artist ( 
+        artist_id VARCHAR(25), 
+        name VARCHAR(100), 
+        location VARCHAR(35), 
+        latitude FLOAT4, 
+        longitude FLOAT4 
+        ); 
+""") 
+ 
+time_table_create = ("""CREATE TABLE time ( 
+        hour INT, 
+        day_of_week INT, 
+        week INT, 
+        month INT, 
+        year INT 
+        ); 
+""") 
+ 
+# INSERT RECORDS 
+ 
+songplay_table_insert = ("""INSERT INTO songplay (timestamp, user_id, level, song_id, artist_id, session_id, location, user_agent) 
+        VALUES(%s, %s, %s, %s, %s, %s, %s, %s); 
+""") 
+ 
+user_table_insert = ("""INSERT INTO users (user_id, first_name, last_name, gender, level) 
+        VALUES(%s, %s, %s, %s, %s); 
+""") 
+ 
+song_table_insert = ("""INSERT INTO song (song_id, title, artist_id, year, duration) 
+        VALUES(%s, %s, %s, %s, %s); 
+""") 
+ 
+artist_table_insert = ("""INSERT INTO artist (artist_id, name, location, latitude, longitude) 
+        VALUES(%s, %s, %s, %s, %s); 
+""") 
+ 
+ 
+time_table_insert = ("""INSERT INTO time (hour, day_of_week, week, month, year) 
+        VALUES(%s, %s, %s, %s, %s); 
+""") 
+ 
+# FIND SONGS 
+ 
+song_select = ("""SELECT song.song_id, song.artist_id FROM song 
+        LEFT JOIN artist ON song.artist_id = artist.artist_id  
+        WHERE song_id = %s AND title = %s AND duration = %s; 
+""") 
+ 
+# QUERY LISTS 
+ 
+create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create] 
 drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
